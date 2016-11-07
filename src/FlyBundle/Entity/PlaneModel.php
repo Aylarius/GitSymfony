270d6c7bcc94d9f1bb95043bca809d6936b7ff26
Thifaine <thifaine.noirault@gmail.com>
2016-11-07 16:05:44 +0100

    MAJ BDD, entity et relations

---
+++ src/FlyBundle/Entity/PlaneModel.php
@@ -0,0 +1,203 @@
+<?php
+
+namespace FlyBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * PlaneModel
+ */
+class PlaneModel
+{
+
+    public function __toString()
+    {
+        return $this->model;
+    }
+
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $model;
+
+    /**
+     * @var string
+     */
+    private $manufacturer;
+
+    /**
+     * @var int
+     */
+    private $cruiseSpeed;
+
+    /**
+     * @var int
+     */
+    private $nbSeats;
+
+    /**
+     * @var string
+     */
+    private $status;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set model
+     *
+     * @param string $model
+     * @return PlaneModel
+     */
+    public function setModel($model)
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    /**
+     * Get model
+     *
+     * @return string 
+     */
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    /**
+     * Set manufacturer
+     *
+     * @param string $manufacturer
+     * @return PlaneModel
+     */
+    public function setManufacturer($manufacturer)
+    {
+        $this->manufacturer = $manufacturer;
+
+        return $this;
+    }
+
+    /**
+     * Get manufacturer
+     *
+     * @return string 
+     */
+    public function getManufacturer()
+    {
+        return $this->manufacturer;
+    }
+
+    /**
+     * Set cruiseSpeed
+     *
+     * @param integer $cruiseSpeed
+     * @return PlaneModel
+     */
+    public function setCruiseSpeed($cruiseSpeed)
+    {
+        $this->cruiseSpeed = $cruiseSpeed;
+
+        return $this;
+    }
+
+    /**
+     * Get cruiseSpeed
+     *
+     * @return integer 
+     */
+    public function getCruiseSpeed()
+    {
+        return $this->cruiseSpeed;
+    }
+
+    /**
+     * Set nbSeats
+     *
+     * @param integer $nbSeats
+     * @return PlaneModel
+     */
+    public function setNbSeats($nbSeats)
+    {
+        $this->nbSeats = $nbSeats;
+
+        return $this;
+    }
+
+    /**
+     * Get nbSeats
+     *
+     * @return integer 
+     */
+    public function getNbSeats()
+    {
+        return $this->nbSeats;
+    }
+
+    /**
+     * Set status
+     *
+     * @param string $status
+     * @return PlaneModel
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return string 
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->model = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add model
+     *
+     * @param \FlyBundle\Entity\Flight $model
+     * @return PlaneModel
+     */
+    public function addModel(\FlyBundle\Entity\Flight $model)
+    {
+        $this->model[] = $model;
+
+        return $this;
+    }
+
+    /**
+     * Remove model
+     *
+     * @param \FlyBundle\Entity\Flight $model
+     */
+    public function removeModel(\FlyBundle\Entity\Flight $model)
+    {
+        $this->model->removeElement($model);
+    }
+}
